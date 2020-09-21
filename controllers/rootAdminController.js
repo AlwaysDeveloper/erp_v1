1ec91ddb9cf7db82f60cb2ba44ce92d1ba49ee26
mukulgupta54738 <mukul.gupta@venturepact.com>
2020-09-21 20:13:24 +0530

    new rootAdminController

---
+++ controllers/rootAdminController.js
@@ -0,0 +1,17 @@
+const User = require('./../models/user.model');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.createUser = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    empId: req.body.empId,
+    accessType: req.body.accessType
+  });
+
+  newUser.password = undefined;
+  
+  res.status(200).json({
+    status: 'success',
+    user: newUser
+  });
+});
