4bc8cf4d09f41f74149c73ede7f2fbd347aac288
mukulgupta54738 <mukul.gupta@venturepact.com>
2020-09-27 19:58:55 +0530

    small changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const AuthHelper = require('../utils/authHelper');
 const catchAsync = require('../utils/catchAsync');
